5731862b86e5ae41d9a3f7d409cb09325eb16a62
SS-93 <Sesughtor@gmail.com>
2023-09-15 09:26:40 -0700

    U4_04 still ticket 4 just pushing the code got the fetch to work git push

--- fakestore.js
+++ fakestore.js
@@ -3,22 +3,25 @@ const navbar = document.querySelector ('.navbar')
 
 const display = document.querySelector('.display') 
 
-let apiURL = ''
+let apiURL = 'https://fakestoreapi.com/products'
 
 
 
  fetch (apiURL) 
  .then (response => response.json())
-   
-.then (data => {
-    console.log(data)
+
+.then (data => displayCards(data))
+
+   .catch (err => console.error(err));
+
+
     //create variable 
 // select element document.create element
 // inner text
 //append child 
 
     
-})
+
 
 // Obtain Data:
 
@@ -38,14 +41,72 @@ let apiURL = ''
 // } // route
 
 // fakeStore ('products') // result 
+function displayCards (data)  {
+
+    let htmlContent = "";
+
+    data.forEach(product => {
+        htmlContent += `
+        <div class="product-box">
+            <img src="${product.image}" class="product-image">
+            <h2>${product.title}</h2>
+            <div class="description-container">
+                <p class="product-description">${product.description}</p>
+                <div class="dropdown-container">
+                    <span class="dropdown-arrow">&#x25BC;</span>
+                    <div class="dropdown-content">
+                        <p>${product.description}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="price-container">
+                <span class="product-price"> $${product.price}</span>
+                <span class="p-dropdown-arrow"> &#x25BC;</span>
+            </div>
+            <button class="add-to-cart"> Add To Cart </button>
+        </div>`;
+    });
+
+    // Set the innerHTML of the display area with the constructed cards
+    document.querySelector('.grid-container').innerHTML = htmlContent;
+}
+
+    
+// const productBoxes = document.querySelectorAll ('product-box');
+
+
+
+// 1. Create new Element(s)
+// 2. edit that new element
+// - adding text
+// -changing styling
+// 3. add the element to the webpage
+
+// data.forEach((product, index)=> {
+
+//     if(productBoxes[index]) {
+//         const box = productBoxes[index]
+//         let title =
+// box.querySelector('h2'),innerText = product.title;  
+// document.body.appendChild  (title)}
+// })
+
+
+// let subHeading = document.createElement('h3');
+
+// subHeading.innerText = 'what should I do before leaving town?';
+// subHeading.style.color = 'pink';
+
+// document.body.appendChild(subHeading);
 
 const fakeStore = async (endpoint) => {
-    let response = await fetch (apiURL + endpoint )
+    let response = await fetch ('https://fakestoreapi.com/products')
     let storeItems = await response.json();
     console.log(storeItems)
+     displayCards (storeItems)
 }
-fakeStore('products')
 
+fakeStore();
 window.onload = function () {
 
     fakeStore ('products');
@@ -59,6 +120,10 @@ categories.forEach(category => {
     category.addEventListener ('click', function (event) {
         event.preventDefault();
 
+
+
         let endpoint = event.currentTarget.getAttribute ('data-endpoint');
         fakeStore(endpoint); })
     });
+
+
